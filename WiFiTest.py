a286aef7da6646143f4e7b45631b72b2ac6261b2
Kasper Helsted <khelst13@student.aau.dk>
2019-04-04 13:00:33 +0200

    Use single Network

--- WiFiTest.py
+++ WiFiTest.py
@@ -1,25 +1,8 @@
-# from time import sleep
-#
-# from WiFi import WiFi
-#
-# device = WiFi("wlan0", "AAU-1x")
-# device.start()
-#
-# try:
-#     while True:
-#         print(device.emit())
-#         sleep(1)
-# except KeyboardInterrupt:
-#     device.stop()
-#
-#
-# from GPIO.Sensors import Sensors
-# from Server import Server
 from Server import Server
 from WiFi import WiFi
 
 # device = WiFi("wlan0", "AAU-1x")
-device = WiFi("wlan0")
+device = WiFi("wlan0", "AAU-1x")
 device.start()
 
 server = Server("0.0.0.0", 20001)
