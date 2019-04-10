2e333302692396cd8cd64d1994fed5e7f5c3a582
Kasper Helsted <khelst13@student.aau.dk>
2019-04-10 11:38:07 +0200

    Updated PORT

--- GPIOTest.py
+++ GPIOTest.py
@@ -4,7 +4,7 @@ from Server import Server
 us = UltrasonicSensor("TEST", 17, 4)
 us.start()
 
-server = Server("0.0.0.0", 20001)
+server = Server("0.0.0.0", 20002)
 server.message_loop.emits(us)
 
 server.start()
